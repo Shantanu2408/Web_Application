d6e56ebb655e2bc6b816bec9873ebf35a5866ccf
Shantanu Singh <shantanusi@infinite.com>
2024-05-08 12:01:42 +0530

    Assesment

---
+++ ASP.NET CORE/Assessments/Assessment1/Assessment1/Repository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Assessment1.Models;
+
+namespace Assessment1.Repository
+{
+    public interface IOrderRepository
+    {
+        public void PlaceOrder(Order order);
+        Order GetOrderDetails(int orderId);
+        public decimal DisplayBill(int orderId);
+        List<Order> GetOrdersByDate(string orderDate);
+        Customer GetCustomerWithHighestOrder();
+    }
+}
